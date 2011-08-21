00d2e8fa5b1d043e3c9331adf998374d6ce52f22
James Polley <jamezpolley@gmail.com>
2011-08-21 13:45:18 +1000

    Tests pass! Data is valid, tests work.
    Added docstring to clarify the intent of Constraints.
    
    Fixed data in tests to be correct, so that the tests have a chance of
    succeeding.

--- constraints.py
+++ constraints.py
@@ -1,6 +1,23 @@
 #!/usrbin/env python
 
 class Constraints:
+    """Returns only those items of a directory which match constraints.
+
+    A directory is a dict of members; each member is itself a dict of
+    attributes. See tests/directory.yaml for a sample
+
+    A constraint is a list of criteria. Each criterion is a dict of attributes;
+    each attribute is a list of potential values for that attribute. See
+    tests/groups.yaml for some sample criteria.
+
+    How do we decide if a member of the directory matches?
+
+    for each criterion in the list of criteria:
+        for each attribute in the criterion's list of attributes:
+            return all members which match any of the possible values
+        return all members which appear on all lists for the criterion
+    return all members which appear on any criterion's list
+    """
 
     def _get_members_for_attr(self, attr, values, directory):
         attr_members = []
@@ -10,27 +27,28 @@ class Constraints:
         return attr_members
 
     def _get_members_for_criterion(self, criterion, directory):
-        criterion_members = set(directory)
-        attr_members = []
+        attr_member_lists = []
+        attr_members = set(directory)
         for attr, values in criterion.items():
-            attr_members.append(self._get_members_for_attr(
+            attr_member_lists.append(self._get_members_for_attr(
                 attr, values, directory))
-        for member_list in attr_members:
-            criterion_members = criterion_members.intersection(
-                    member_list)
+        for attr_member_list in attr_member_lists:
+            print 'am,aml', attr_members, attr_member_list
+            attr_members = attr_members.intersection(attr_member_list)
+        return attr_members
+
+    def _get_members_for_criteria(self, criteria, directory):
+        criterion_member_lists = []
+        criterion_members = set()
+        for criterion in criteria:
+            criterion_member_lists.append(self._get_members_for_criterion(
+                criterion, directory))
+        for criterion_member_list in criterion_member_lists:
+            print 'cm, cml', criterion_members, criterion_member_list
+            criterion_members = criterion_members.union(criterion_member_list)
         return criterion_members
-
-
 
     def get_members(self, criteria, directory=None):
         directory = directory or self._directory
-        members = set()
-        for criterion in criteria:
-            members = members.union(
-                    self._get_members_for_criterion(criterion, directory))
-
+        members = self._get_members_for_criteria(criteria, directory)
         return members
-
-
-
-
--- tests/constraints_tests.py
+++ tests/constraints_tests.py
@@ -16,23 +16,30 @@ class TestConstraints(object):
 
         cls.constrainer = Constraints()
 
-    def test_members_for_attr(self):
-        attr = 'location'
-        values = ['Winterfell', 'Kings Landing']
-        expected_members = ['eddard', 'robert', 'catelyn', 'cersei', 'tyrion']
-        generated_members = self.constrainer._get_members_for_attr(
-                attr, values, self.directory)
+    def compare_members_lists(self, expected_members, generated_members):
         assert len(generated_members) == len(expected_members)
         for expected_member in expected_members:
             assert expected_member in generated_members
+
+    def test_members_for_attr(self):
+        attr = 'location'
+        values = ['Free Cities', 'Kings Landing']
+        expected_members = ['robert', 'cersei', 'tyrion', 'viserys', 'daenrys']
+        generated_members = self.constrainer._get_members_for_attr(
+                attr, values, self.directory)
+        self.compare_members_lists(expected_members, generated_members)
+
+    def test_members_for_criterion(self):
+        criterion = { 'location': ['Winterfell'], 'department': ['Tully'] }
+        expected_members = ['catelyn']
+        generated_members = self.constrainer._get_members_for_criterion(
+                criterion, self.directory)
+        self.compare_members_lists(expected_members, generated_members)
 
     def check_group_membership(self, criteria, expected_members):
         generated_members = self.constrainer.get_members(criteria=criteria,
                 directory=self.directory)
-        assert len(generated_members) == len(expected_members)
-        for expected_member in expected_members:
-            assert expected_member in generated_members
-
+        self.compare_members_lists(expected_members, generated_members)
 
     def test_group_memberships(self):
         for test_name, test_data in self.groups.items():
