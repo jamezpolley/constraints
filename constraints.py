b1b142fdaed81357f17727decc08c524d7a5a671
James Polley <jamezpolley@gmail.com>
2011-08-21 10:39:11 +1000

    Test for _get_members_for_attr shows that it works

--- constraints.py
+++ constraints.py
@@ -2,14 +2,32 @@
 
 class Constraints:
 
+    def _get_members_for_attr(self, attr, values, directory):
+        attr_members = []
+        for member_name, member in directory.items():
+            if (attr in member and member[attr] in values):
+                attr_members.append(member_name)
+        return attr_members
+
+    def _get_members_for_criterion(self, criterion, directory):
+        criterion_members = set(directory)
+        attr_members = []
+        for attr, values in criterion.items():
+            attr_members.append(self._get_members_for_attr(
+                attr, values, directory))
+        for member_list in attr_members:
+            criterion_members = criterion_members.intersection(
+                    member_list)
+        return criterion_members
+
+
+
     def get_members(self, criteria, directory=None):
+        directory = directory or self._directory
         members = set()
         for criterion in criteria:
-            criterion_members = set()
-            for member_name, member in directory.items():
-                for attr, values in criterion.items():
-                    if (attr in member and member[attr] in values):
-                        attr_members.add(member_name)
+            members = members.union(
+                    self._get_members_for_criterion(criterion, directory))
 
         return members
 
--- tests/constraints_tests.py
+++ tests/constraints_tests.py
@@ -16,6 +16,16 @@ class TestConstraints(object):
 
         cls.constrainer = Constraints()
 
+    def test_members_for_attr(self):
+        attr = 'location'
+        values = ['Winterfell', 'Kings Landing']
+        expected_members = ['eddard', 'robert', 'catelyn', 'cersei', 'tyrion']
+        generated_members = self.constrainer._get_members_for_attr(
+                attr, values, self.directory)
+        assert len(generated_members) == len(expected_members)
+        for expected_member in expected_members:
+            assert expected_member in generated_members
+
     def check_group_membership(self, criteria, expected_members):
         generated_members = self.constrainer.get_members(criteria=criteria,
                 directory=self.directory)
