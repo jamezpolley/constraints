7b6e401dc79c573dbe5a48afb1d6b4ac3578cf2f
James Polley <jamezpolley@gmail.com>
2011-08-22 21:37:34 +1000

    Add manifest and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup (
+        name = 'constraints',
+        packages = ['constraints'],
+        version = '0.9.0',
+        description = 'Returns a constrained subset of the members of a dict of dicts',
+        author = 'James Polley',
+        author_email = 'jamezpolley@gmail.com',
+        url = 'http://pypi.python.org/pypi/contraints/',
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+        requires = [
+            'PyYAML',
+            'unittest2'
+        ],
+
+)
